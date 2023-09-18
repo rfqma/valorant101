0bbf076e47184171e914650597cbd61d5611ba74
Rifqi Maulana <abelcaster025@gmail.com>
2023-09-18 17:11:45 +0700

    fix: agent page max width to implement mobile layout only

--- src/app/agent/page.tsx
+++ src/app/agent/page.tsx
@@ -3,7 +3,7 @@ import Agent from "./Agent"
 export default function AgentPage() {
     return (
         <>
-            <div className='container p-10'>
+            <div className='container p-5 max-w-lg'>
                 <Agent />
             </div>
         </>
