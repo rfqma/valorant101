669c2565afa335eb144b081fcd899bb58a70895a
Rifqi Maulana <abelcaster025@gmail.com>
2023-09-18 21:51:44 +0700

    fix: home page display

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import Link from "next/link"
-
 import Image from "next/image"
 
 export default function Home() {
@@ -8,18 +6,6 @@ export default function Home() {
       <div className='container bg-gray-800 max-w-lg min-h-screen'>
         <div className='container p-10'>
           <div className='flex flex-col p-6 gap-5 items-center'>
-            <div className='flex flex-col text-center gap-3'>
-              <Link href={'/agent'}>
-                <div className='rounded-2xl bg-gray-900 px-5 py-2 shadow-2xl'>
-                  <span className='font-poppins font-semibold text-sm text-gray-300'>Agent Page</span>
-                </div>
-              </Link>
-              <Link href={'/map'}>
-                <div className='rounded-2xl bg-gray-900 px-5 py-2 shadow-2xl'>
-                  <span className='font-poppins font-semibold text-sm text-gray-300'>Map Page</span>
-                </div>
-              </Link>
-            </div>
             <div className='flex flex-col text-center items-center gap-3'>
               <p className='font-poppins font-normal text-sm text-gray-300'>
                 aplikasi webnye blom optimal ges, masih under development.
