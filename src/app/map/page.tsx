4d34144f335b8a0e570afb78b0242e617e991588
Rifqi Maulana <abelcaster025@gmail.com>
2023-09-18 22:01:12 +0700

    fix: slight display change at map page

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,7 +1,9 @@
 export default function MapPage() {
     return (
-        <div>
-            <span>under construction</span>
+        <div className='container bg-gray-800 max-w-lg min-h-screen'>
+            <div className='container p-5'>
+                <span className='font-poppins font-normal text-sm text-gray-300'>under construction ya pooh sepooh</span>
+            </div>
         </div>
     )
 }
