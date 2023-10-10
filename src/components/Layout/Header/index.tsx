e57f4835ef331eba5d3b1e5df1224fa446231949
Rifqi Maulana <abelcaster025@gmail.com>
2023-10-10 19:42:25 +0700

    add: hamburger menu

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -1,6 +1,16 @@
+'use client'
+
 import Link from "next/link"
+import { useState } from "react"
+import { GiHamburgerMenu } from 'react-icons/gi'
 
 export default function Header() {
+    const [headerOpen, setHeaderOpen] = useState<boolean>(false)
+
+    const handleHeaderToggler = () => {
+        setHeaderOpen(!headerOpen)
+    }
+
     return (
         <>
             <div className='bg-white'>
@@ -11,13 +21,31 @@ export default function Header() {
                                 <h1 className='text-2xl font-extrabold text-black'>VAL 101</h1>
                             </Link>
                         </div>
-                        <div className='flex gap-8'>
-                            <Link href={'/agent'}>
-                                <span className='text-base font-semibold text-darkGreen hover:text-black'>Agent</span>
-                            </Link>
-                            <Link href={'/map'}>
-                                <span className='text-base font-semibold text-darkGreen hover:text-black'>Map</span>
-                            </Link>
+                        <div>
+                            <button
+                                onClick={handleHeaderToggler}
+                                className=""
+                            >
+                                <GiHamburgerMenu />
+                            </button>
+                            {
+                                headerOpen ?
+                                    <div className="fixed right-7 bg-black shadow-lg border rounded-md p-4">
+                                        <div className="flex flex-col gap-2">
+                                            <Link href={'/agent'} className="border-b border-body-color">
+                                                <span className='text-sm font-normal text-white hover:text-darkGreen'>Agent</span>
+                                            </Link>
+                                            <Link href={'/map'} className="border-b border-body-color">
+                                                <span className='text-sm font-normal text-white hover:text-darkGreen'>Map</span>
+                                            </Link>
+                                            <Link href={'/weapon'} className="border-b border-body-color">
+                                                <span className='text-sm font-normal text-white hover:text-darkGreen'>Weapon</span>
+                                            </Link>
+                                        </div>
+                                    </div>
+                                    :
+                                    null
+                            }
                         </div>
                     </div>
                 </div>
