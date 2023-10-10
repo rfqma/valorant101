bf333b2a35cab82cee14b8c80a5c7129cff0386b
Rifqi Maulana <abelcaster025@gmail.com>
2023-10-10 22:10:12 +0700

    add: additional header handle

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -1,14 +1,27 @@
 'use client'
 
 import Link from "next/link"
-import { useState } from "react"
+import { useState, useRef, useEffect } from "react"
 import { GiHamburgerMenu } from 'react-icons/gi'
 
 export default function Header() {
     const [headerOpen, setHeaderOpen] = useState<boolean>(false)
+    const menuRef = useRef<HTMLDivElement>(null)
 
     const handleHeaderToggler = () => {
         setHeaderOpen(!headerOpen)
+    }
+
+    const handleHeaderClickEverywhere = (event: any) => {
+        if (menuRef.current && !menuRef.current.contains(event.target)) {
+            setHeaderOpen(false)
+        }
+    }
+
+    if (headerOpen) {
+        document.addEventListener('mousedown', handleHeaderClickEverywhere)
+    } else {
+        document.removeEventListener('mousedown', handleHeaderClickEverywhere)
     }
 
     return (
@@ -30,7 +43,7 @@ export default function Header() {
                             </button>
                             {
                                 headerOpen ?
-                                    <div className="fixed right-7 bg-black shadow-lg border rounded-md p-4">
+                                    <div id="hamburgerMenu" ref={menuRef} className="fixed right-7 bg-black shadow-lg border rounded-md p-4">
                                         <div className="flex flex-col gap-2">
                                             <Link href={'/agent'} className="border-b border-body-color">
                                                 <span className='text-sm font-normal text-white hover:text-darkGreen'>Agent</span>
